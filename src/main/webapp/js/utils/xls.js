87714d43e6a7b4935ef362363cea79187b7b08c2
Nevercome <sunss1903@163.com>
2018-10-15 21:00:41 +0800

    batch register and single register, not test

---
+++ src/main/webapp/js/utils/xls.js
@@ -0,0 +1,41 @@
+// <input type="file" id="excel-file">
+
+var datas = [];
+
+function chooseXLSX() {
+
+    $('#excel-file').change(function (e) {
+        var files = e.target.files;
+
+        var fileReader = new FileReader();
+        fileReader.onload = function (ev) {
+            try {
+                var data = ev.target.result,
+                    workbook = XLSX.read(data, {
+                        type: 'binary'
+                    }) // 以二进制流方式读取得到整份excel表格对象
+            } catch (e) {
+                console.log('文件类型不正确');
+                return;
+            }
+            // 表格的表格范围，可用于判断表头是否数量是否正确
+            var fromTo = '';
+            // 遍历每张表读取
+            for (var sheet in workbook.Sheets) {
+                if (workbook.Sheets.hasOwnProperty(sheet)) {
+                    fromTo = workbook.Sheets[sheet]['!ref'];
+                    console.log(fromTo);
+                    datas = datas.concat(XLSX.utils.sheet_to_json(workbook.Sheets[sheet]));
+                    // break; // 如果只取第一张表，就取消注释这行
+                }
+            }
+            console.log(datas);
+        };
+        // 以二进制方式打开文件
+        fileReader.readAsBinaryString(files[0]);
+    });
+}
+
+function getReadResult() {
+    return datas;
+}
